96dffd49bd8c3ebe7e727967ea59295353d7e688
Sergii703 <5121988zZ>
2018-03-23 15:20:57 +0100

    start

---
+++ game.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Space Invaders")
+
+while True:  # игровой цикл
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # обработка события
+            sys.exit(0)  # завершение приложения
+
+
+
+
