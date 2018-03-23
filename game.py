5aec92967daf070cc02b76fa1d9f67fc95b1dadc
Sergii703 <5121988zZ>
2018-03-23 15:46:22 +0100

    start1

--- game.py
+++ game.py
@@ -1,17 +1,20 @@
 import pygame
 import sys
 
+from settings import SIZE, WHITE
+
 pygame.init()
-
-
-screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Space Invaders")
+
+screen = pygame.display.set_mode(SIZE)  # импортируем настройки
 
 while True:  # игровой цикл
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # обработка события
             sys.exit(0)  # завершение цикла
+    screen.fill(WHITE)  # изменение цвета экрана
 
+    pygame.display.flip()
 
 
 
