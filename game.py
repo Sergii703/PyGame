521ae0d8a89473970dd7092266e6e944b69151ce
Sergii <sergey703@gmail.com>
2018-03-23 15:34:10 +0100

    Update game.py
    ыефке2

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Space Invaders")
 while True:  # игровой цикл
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # обработка события
-            sys.exit(0)  # завершение
+            sys.exit(0)  # завершение цикла
 
 
 
