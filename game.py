73c6ad4e81f27a0ac1ade9109cb8b1669aa767e9
Sergii703 <5121988zZ>
2018-03-29 16:06:54 +0200

    set up background

--- game.py
+++ game.py
@@ -1,17 +1,20 @@
 import pygame
 import sys
 
-from game_objects import Player
+from game_objects import Player, Background
 from settings import SIZE, WHITE
 
 pygame.init()
 pygame.display.set_caption("Space Invaders")
 
 screen = pygame.display.set_mode(SIZE)  # импортируем настройки
+clock = pygame.time.Clock()  # что бы быстро не перемещался
+
+
 
 # отображение класса игрока
 player = Player()
-
+background = Background()
 
 while True:  # игровой цикл
     for event in pygame.event.get():
@@ -21,10 +24,13 @@ while True:  # игровой цикл
     screen.fill(WHITE)  # изменение цвета экрана
 
     player.update()
+    background.update()
 
+    screen.blit(background.image, background.rect)  # отображение фона
     screen.blit(player.image, player.rect)  # отображение игрока
 
-    pygame.display.flip()
 
+    pygame.display.flip()
+    clock.tick(30)  # скорость обновления экрана
 
 
--- game_objects.py
+++ game_objects.py
@@ -3,7 +3,10 @@ import pygame
 from settings import WIDTH, HEIGHT
 
 
-class Player(pygame.sprite.Sprite):  #класс игрока
+class Player(pygame.sprite.Sprite):  # класс игрока
+
+    max_speed = 10
+
     def __init__(self):
         super(Player, self).__init__()
 
@@ -13,10 +16,31 @@ class Player(pygame.sprite.Sprite):  #класс игрока
         self.rect.centerx = WIDTH / 2  # его располодение
         self.rect.bottom = HEIGHT - 10  # его расположения
 
-        def update(self):
-            keys = pygame.key.get_pressed()
+        self.current_speed = 0
 
-            if keys[pygame.K_LEFT]:  # клавиша лево
-                print('LEFT')
-            elif keys[pygame.K_RIGHT]:  # клавиша право
-                print('RIGHT')
+    def update(self):  #  функция перемещения игрока
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:  # клавиша лево
+            self.current_speed = -self.max_speed
+        elif keys[pygame.K_RIGHT]:  # клавиша право
+            self.current_speed = self.max_speed
+        else:
+            self.current_speed = 0
+
+        self.rect.move_ip((self.current_speed, 0))
+
+class Background (pygame.sprite.Sprite):  # фон
+    def __init__ (self):
+        super(Background, self).__init__()
+
+        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/stars.jpeg')
+        self.rect = self.image.get_rect()  #размер и располодение фона
+
+        self.rect.bottom = HEIGHT
+
+    def update(self):
+        self.rect.bottom += 5
+
+        if self.rect.bottom >= self.rect.height:
+            self.rect.bottom = HEIGHT
