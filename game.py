01e1684fc77801eef87cbc52e716887c811f5449
Sergii703 <5121988zZ>
2018-04-06 16:52:34 +0200

    add shooting

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-from game_objects import Player, Background
+from game_objects import Player, Background, Shoot
 from settings import SIZE, WHITE
 
 pygame.init()
@@ -10,11 +10,17 @@ pygame.display.set_caption("Space Invaders")
 screen = pygame.display.set_mode(SIZE)  # импортируем настройки
 clock = pygame.time.Clock()  # что бы быстро не перемещался
 
-
-
 # отображение класса игрока
-player = Player()
+player = Player()  # присваеваем объекты к переменным
 background = Background()
+# shoot = Shoot()
+
+# Группв создается для того чтобы не лписывать каждый объкет по отдельности
+all_objects = pygame.sprite.Group()
+
+all_objects.add(background)
+all_objects.add(player)
+all_objects.add(Shoot(player.rect.midtop))
 
 while True:  # игровой цикл
     for event in pygame.event.get():
@@ -23,12 +29,13 @@ while True:  # игровой цикл
 
     screen.fill(WHITE)  # изменение цвета экрана
 
-    player.update()
-    background.update()
+    # player.update()
+    # background.update()
+    all_objects.update()
 
-    screen.blit(background.image, background.rect)  # отображение фона
-    screen.blit(player.image, player.rect)  # отображение игрока
-
+    # screen.blit(background.image, background.rect)  # отображение фона
+    # screen.blit(player.image, player.rect)  # отображение игрока
+    all_objects.draw(screen)  # что ьы не отображать по одному объекту сразу отображаем группу
 
     pygame.display.flip()
     clock.tick(30)  # скорость обновления экрана
--- game_objects.py
+++ game_objects.py
@@ -3,6 +3,17 @@ import pygame
 from settings import WIDTH, HEIGHT
 
 
+class Shoot(pygame.sprite.Sprite):
+
+    def __init__(self, position):
+        super(Shoot, self).__init__()
+
+        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/shoot.png')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = position
+
+
 class Player(pygame.sprite.Sprite):  # класс игрока
 
     max_speed = 10
@@ -10,7 +21,7 @@ class Player(pygame.sprite.Sprite):  # класс игрока
     def __init__(self):
         super(Player, self).__init__()
 
-        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/space.jpeg')
+        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/spaceShip.png')
         self.rect = self.image.get_rect()
 
         self.rect.centerx = WIDTH / 2  # его располодение
@@ -18,7 +29,7 @@ class Player(pygame.sprite.Sprite):  # класс игрока
 
         self.current_speed = 0
 
-    def update(self):  #  функция перемещения игрока
+    def update(self):  # функция перемещения игрока
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:  # клавиша лево
@@ -30,16 +41,17 @@ class Player(pygame.sprite.Sprite):  # класс игрока
 
         self.rect.move_ip((self.current_speed, 0))
 
+
 class Background (pygame.sprite.Sprite):  # фон
-    def __init__ (self):
+    def __init__(self):
         super(Background, self).__init__()
 
-        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/stars.jpeg')
-        self.rect = self.image.get_rect()  #размер и располодение фона
+        self.image = pygame.image.load('/home/kovals/PycharmProjects/PyGame/game/stars.png')
+        self.rect = self.image.get_rect()  # размер и располодение фона
 
         self.rect.bottom = HEIGHT
 
-    def update(self):
+    def update(self):  # иллюзия движения
         self.rect.bottom += 5
 
         if self.rect.bottom >= self.rect.height:
